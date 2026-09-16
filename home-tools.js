d9311bbd7823d1c124cea2067555c9847767f33f
olgapopovaphone-web <olgapopovaphone@gmail.com>
2026-09-16 12:56:29 +0300

    Bump Afisha discovery cache after menu removal

--- home-tools.js
+++ home-tools.js
@@ -31,6 +31,6 @@
   import('/community-detail.js?v=20260916-2');
   import('/community-detail-link.js?v=20260916-2');
   import('/event-group-prototype.js?v=20260916-1').then(function(){return import('/event-edit.js?v=20260916-1')});
-  import('/place-detail.js?v=20260915-2').then(function(){return import('/home-discovery.js?v=20260915-2')});
+  import('/place-detail.js?v=20260915-2').then(function(){return import('/home-discovery.js?v=20260916-3')});
   import('/brand-lya.js?v=20260916-1');
 })();
