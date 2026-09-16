7873f35927fb4f93a7e893bbfb527cc91e0c358e
olgapopovaphone-web <olgapopovaphone@gmail.com>
2026-09-16 22:38:45 +0300

    Load contextual app invite in people search

--- home-tools.js
+++ home-tools.js
@@ -1,5 +1,5 @@
 (function(){
-  ['/home-discovery.css?v=20260915-2','/around-people.css?v=20260916-1','/place-detail.css?v=20260915-2','/my-events.css?v=20260915-2','/social-hub.css?v=20260916-3','/social-circle.css?v=20260916-2','/social-cleanup.css?v=20260916-1','/people-tab-v2.css?v=20260916-1','/navigation-order-v2.css?v=20260916-1','/event-group-prototype.css?v=20260916-1','/event-edit.css?v=20260916-1','/event-circle-invite.css?v=20260916-1','/community-detail.css?v=20260916-1','/brand-lya.css?v=20260916-2','/brand-color-balance.css?v=20260916-2','/brand-vokrug.css?v=20260916-5','/image-cropper.css?v=20260916-2'].forEach(function(href){var l=document.createElement('link');l.rel='stylesheet';l.href=href;document.head.appendChild(l)});
+  ['/home-discovery.css?v=20260915-2','/around-people.css?v=20260916-1','/place-detail.css?v=20260915-2','/my-events.css?v=20260915-2','/social-hub.css?v=20260916-3','/social-circle.css?v=20260916-2','/social-cleanup.css?v=20260916-1','/people-tab-v2.css?v=20260916-1','/people-invite-app.css?v=20260916-1','/navigation-order-v2.css?v=20260916-1','/event-group-prototype.css?v=20260916-1','/event-edit.css?v=20260916-1','/event-circle-invite.css?v=20260916-1','/community-detail.css?v=20260916-1','/brand-lya.css?v=20260916-2','/brand-color-balance.css?v=20260916-2','/brand-vokrug.css?v=20260916-5','/image-cropper.css?v=20260916-2'].forEach(function(href){var l=document.createElement('link');l.rel='stylesheet';l.href=href;document.head.appendChild(l)});
 
   function simplifyTabHeaders(){
     var createTop=document.querySelector('[data-view="create"] .top');
@@ -28,7 +28,7 @@
   import('/home-core.js?v=20260916-4');
   import('/calendar-reminders.js?v=20260915-2');
   import('/my-events.js?v=20260915-1').then(function(){simplifyTabHeaders()});
-  import('/social-hub.js?v=20260916-6').then(function(){return import('/circle-labels.js?v=20260916-2')}).then(function(){return import('/social-cleanup.js?v=20260916-1')}).then(function(){return import('/people-tab-v2.js?v=20260916-1')});
+  import('/social-hub.js?v=20260916-6').then(function(){return import('/circle-labels.js?v=20260916-2')}).then(function(){return import('/social-cleanup.js?v=20260916-1')}).then(function(){return import('/people-tab-v2.js?v=20260916-1')}).then(function(){return import('/people-invite-app.js?v=20260916-1')});
   import('/community-detail.js?v=20260916-2');
   import('/community-detail-link.js?v=20260916-2');
   import('/event-group-prototype.js?v=20260916-1').then(function(){return import('/event-edit.js?v=20260916-1')}).then(function(){return import('/event-circle-invite.js?v=20260916-2')});
