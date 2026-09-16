f96057177c14dbe04498c93211748fe132962ca0
olgapopovaphone-web <olgapopovaphone@gmail.com>
2026-09-16 16:58:22 +0300

    Restore working navigation behavior before menu reorder

--- home-tools.js
+++ home-tools.js
@@ -1,5 +1,5 @@
 (function(){
-  ['/home-discovery.css?v=20260915-2','/around-people.css?v=20260916-1','/place-detail.css?v=20260915-2','/my-events.css?v=20260915-2','/social-hub.css?v=20260916-3','/social-circle.css?v=20260916-2','/navigation-order.css?v=20260916-1','/event-group-prototype.css?v=20260916-1','/event-edit.css?v=20260916-1','/community-detail.css?v=20260916-1','/brand-lya.css?v=20260916-2','/brand-color-balance.css?v=20260916-2','/brand-vokrug.css?v=20260916-5','/image-cropper.css?v=20260916-2'].forEach(function(href){var l=document.createElement('link');l.rel='stylesheet';l.href=href;document.head.appendChild(l)});
+  ['/home-discovery.css?v=20260915-2','/around-people.css?v=20260916-1','/place-detail.css?v=20260915-2','/my-events.css?v=20260915-2','/social-hub.css?v=20260916-3','/social-circle.css?v=20260916-2','/event-group-prototype.css?v=20260916-1','/event-edit.css?v=20260916-1','/community-detail.css?v=20260916-1','/brand-lya.css?v=20260916-2','/brand-color-balance.css?v=20260916-2','/brand-vokrug.css?v=20260916-5','/image-cropper.css?v=20260916-2'].forEach(function(href){var l=document.createElement('link');l.rel='stylesheet';l.href=href;document.head.appendChild(l)});
 
   function simplifyTabHeaders(){
     var createTop=document.querySelector('[data-view="create"] .top');
