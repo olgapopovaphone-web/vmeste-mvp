dcb33329cd41148c6281705dcf5481b235e61da9
olgapopovaphone-web <olgapopovaphone@gmail.com>
2026-09-18 01:12:54 +0300

    Finalize image system loading order

--- home-tools.js
+++ home-tools.js
@@ -35,5 +35,5 @@
   import('/place-detail.js?v=20260915-2').then(function(){return import('/home-discovery.js?v=20260916-3')}).then(function(){return import('/around-people.js?v=20260916-1')});
   import('/chronicle-v2.js?v=20260916-1');
   import('/event-media.js?v=20260918-2');
-  import('/brand-lya.js?v=20260917-1').then(function(){return import('/header-kit-v1.js?v=20260918-1')}).then(function(){return import('/create-landing-v2.js?v=20260917-3')}).then(function(){return import('/community-create-submit-bridge.js?v=20260918-2')}).then(function(){return import('/community-create-link-bridge.js?v=20260917-1')}).then(function(){return import('/image-system-v2.js?v=20260918-2')});
+  import('/brand-lya.js?v=20260917-1').then(function(){return import('/header-kit-v1.js?v=20260918-1')}).then(function(){return import('/create-landing-v2.js?v=20260917-3')}).then(function(){return import('/community-create-submit-bridge.js?v=20260918-2')}).then(function(){return import('/community-create-link-bridge.js?v=20260917-1')}).then(function(){return import('/image-system-v2.js?v=20260918-2')}).then(function(){return import('/image-system-reset-fix.js?v=20260918-1')});
 })();
