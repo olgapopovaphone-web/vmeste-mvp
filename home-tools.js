6f5ff9a29d06a9b09ae7fc2149f53b001b996509
olgapopovaphone-web <olgapopovaphone@gmail.com>
2026-09-17 22:41:54 +0300

    Bump completed create flow asset

--- home-tools.js
+++ home-tools.js
@@ -35,5 +35,5 @@
   import('/place-detail.js?v=20260915-2').then(function(){return import('/home-discovery.js?v=20260916-3')}).then(function(){return import('/around-people.js?v=20260916-1')});
   import('/chronicle-v2.js?v=20260916-1');
   import('/event-media.js?v=20260917-2').then(function(){return import('/event-media-upload-fix.js?v=20260917-1')});
-  import('/brand-lya.js?v=20260917-1').then(function(){return import('/header-kit-v1.js?v=20260917-1')}).then(function(){return import('/create-landing-v2.js?v=20260917-2')});
+  import('/brand-lya.js?v=20260917-1').then(function(){return import('/header-kit-v1.js?v=20260917-1')}).then(function(){return import('/create-landing-v2.js?v=20260917-3')});
 })();
